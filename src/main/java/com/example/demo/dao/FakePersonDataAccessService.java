5bdcfc3f8390ad687abdb33703bba17b82e2e7f0
stefanB <stefanbolcs@gmail.com>
2020-05-29 19:51:31 +0200

    getById with {id}

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 //so its a bean that can be injected
@@ -25,5 +26,22 @@ public class FakePersonDataAccessService implements PersonDAO {
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person-> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
     }
 }
